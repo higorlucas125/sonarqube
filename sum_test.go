3ae5756f08ff0a6c79958a8e9a2bad556a2c92f1
higorlucas125 <higorlaf@hotmail.com>
2023-01-21 12:18:01 +0000

    ci: insert new testing in sum_test.go

--- sum_test.go
+++ sum_test.go
@@ -9,3 +9,27 @@ func TestSoma(t *testing.T){
 		t.Errorf("Resultado da soma é inválido: Resultado %d. Esperado: %d",total,30)
 	}
 }
+
+func TestSub(t *testing.T){
+	total := Sub(5,2)
+
+	if total != 3 {
+		t.Errorf("Resultado da subtração é inválido: Resultado %d. Esperado: %d",total,30)
+	}
+}
+
+func TestTimes(t *testing.T){
+	total := Times(10,5)
+
+	if total != 2 {
+		t.Errorf("Resultado da divisão é inválido: Resultado %d. Esperado: %d",total,30)
+	}
+}
+
+func TestSumX(t *testing.T){
+	total := SumX(2,5)
+
+	if total != 10 {
+		t.Errorf("Resultado da multiplicação é inválido: Resultado %d. Esperado: %d",total,30)
+	}
+}
